45943278c51c89c27b460fa887980903c4ec7e61
soro adama <braintech0321@gmail.com>
2024-03-02 11:14:11 +0000

    v5.1

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const port =5000
 
 app.use(bodyParser.json()) ;
 
- app.get('api/v1/assure', (req,res)=> AssureControllers.getAllAssure(req,res));
- app.get('api/v1/assure:id',(req,res)=>AssureControllers.getAssureById(req, res));
- app.put('api/v1/assure:id',(req,res)=>AssureControllers.updatAssure(req,res));
- app.post('api/v1/assure',(req,res)=>AssureControllers.createAssure(req,res));
+ app.get('/api/v1/assure', (req,res)=> AssureControllers.getAllAssure(req,res));
+ app.get('/api/v1/assure:id',(req,res)=>AssureControllers.getAssureById(req, res));
+ app.put('/api/v1/assure:id',(req,res)=>AssureControllers.updatAssure(req,res));
+ app.post('/api/v1/assure',(req,res)=>AssureControllers.createAssure(req,res));
  //           
 app.listen(port,()=>{console.log("app is running on port "+port)})
